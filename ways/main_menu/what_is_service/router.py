a15ebffccf05f2313de9dacae429194d564086c9
TheDigitalMadness <zakhar.burov050209@yandex.ru>
2025-09-23 23:37:46 +0300

    logging

--- ways/main_menu/what_is_service/router.py
+++ ways/main_menu/what_is_service/router.py
@@ -1,3 +1,5 @@
+import logging
+
 from aiogram import Router, F
 from aiogram.types import CallbackQuery
 from aiogram.fsm.context import FSMContext
@@ -21,7 +23,7 @@ async def how_we_work(callback: CallbackQuery, state: FSMContext):
 
 @router.callback_query(cfg.WhatIsServiceWay.choose_action, F.data == "send_start_pdf")
 async def send_start_pdf(callback: CallbackQuery, state: FSMContext):
-    print(app_cfg.Files.START_SELL_PDF)
+    logging.log(app_cfg.Files.START_SELL_PDF)
     await callback.message.answer_document(FSInputFile(app_cfg.Files.START_SELL_PDF))
 
 
