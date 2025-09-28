2bbdab3df73952385e1238cd9430baba6edd1d1f
TheDigitalMadness <zakhar.burov050209@yandex.ru>
2025-09-28 16:45:53 +0300

    logging

--- bot.py
+++ bot.py
@@ -17,7 +17,8 @@ logging.basicConfig(
     handlers=[
         logging.FileHandler("bot.log", encoding="utf-8"),
         logging.StreamHandler()
-    ]
+    ],
+    force=True,
 )
 logger = logging.getLogger(__name__)
 
